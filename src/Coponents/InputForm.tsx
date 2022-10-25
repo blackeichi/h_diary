84eeaa1250ca5a655b80ba599b9758b263f75ba9
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-10-25 17:51:32 +0900

    Update InputForm.tsx

--- src/Coponents/InputForm.tsx
+++ src/Coponents/InputForm.tsx
@@ -1,3 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const Input = styled.input`
+  background-color: ${(props) => props.theme.blackColr};
+  color: white;
+`;
+
 export const InputForm = () => {
-  return null;
+  return (
+    <Box>
+      <Form>
+        <Input></Input>
+        <Input></Input>
+      </Form>
+    </Box>
+  );
 };
