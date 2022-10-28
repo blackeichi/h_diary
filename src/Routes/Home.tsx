e8a0254fc5cef5be381b2c4f564606cee37f4ba0
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-10-28 14:46:14 +0900

    Update Home.tsx

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import { authService } from "../fbase";
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+const Container = styled.div``;
 
 export const Home = () => {
   const LogOut = () => {
     authService.signOut();
   };
-  return <h1 onClick={LogOut}>home</h1>;
+  return (
+    <Box>
+      <h1 onClick={LogOut}>home</h1>
+      <Container></Container>
+    </Box>
+  );
 };
